5c4232394cb08a69858388a43fae81af3139b8f6
yileon-ada <114987386+yileon-ada@users.noreply.github.com>
2023-01-17 12:02:47 +0100

    2en1

--- word_replace.js
+++ word_replace.js
@@ -30,3 +30,24 @@ for(let i=0; i<text.length; i++){
         replace( 'bâtard ', '\ud83d\udca9')
     }
 }
+
+
+const articles = document.querySelectorAll("div.mw-parser-output > p");
+
+articles.forEach(article => {
+  if (article) {
+    const text = article.textContent;
+    const wordMatchRegExp = /[^\s]+/g; // Regular expression
+    const words = text.matchAll(wordMatchRegExp);
+    // matchAll returns an iterator, convert to array to get word count
+    const wordCount = [...words].length;
+    const readingTime = Math.round(wordCount / 200);
+    const badge = document.createElement("p");
+    // Use the same styling as the publish information in an article's header
+    badge.classList.add("color-secondary-text", "type--caption");
+    badge.textContent = `⏱️ ${readingTime} min read`;
+
+    article.insertAdjacentElement("afterend", badge);
+  }
+});
+  
